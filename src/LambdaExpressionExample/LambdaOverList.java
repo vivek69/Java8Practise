22f336edfc5b225b4609d5ca9e238ee49bb2b938
vivek Bhardwaj <VIVEK@Lenovo-PC>
2021-10-02 13:59:12 +0530

    second comitt

--- src/LambdaExpressionExample/LambdaOverList.java
+++ src/LambdaExpressionExample/LambdaOverList.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class LambdaOverList {
 public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	List<String> ab=Arrays.asList("tjis","is");
+	List<String> ab=Arrays.asList("this","is");
 	
 	ab.forEach(p->System.out.println(p));
 
